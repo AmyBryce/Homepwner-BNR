bdd66df6d10d681f47de9a875b975d282e7dccf2
AmyBryce <a.bryce9@gmail.com>
2013-07-02 22:42:17 -0700

    first commit

---
+++ Homepwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Amy Bryce on 7/2/13.
+//  Copyright (c) 2013 Amy Bryce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Amy Bryce on 7/2/13.
+//  Copyright (c) 2013 Amy Bryce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
